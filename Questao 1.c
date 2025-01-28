fdd541fbf58b7d96bdd684894bea2de4459d5022
VICTOR GABRIEL DE JESUS SANTANA <149277458+vgsantana@users.noreply.github.com>
2025-01-28 20:07:58 -0300

    adicionar arquivo

---
+++ Questao 1.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+void main()
+{
+    float n;
+    printf("Digite um numero: ");
+    scanf("%f", &n);
+    
+    if (n > 0){
+        printf("%.2f é Positivo", n);
+    } else if(n < 0){
+        printf("%.2f é Negativo", n);
+    } else {
+        printf("%.2f é Igual a zero", n);
+    }
+
+}
